fbfd8ca206a60be0aebbbf6c9b8fccb6efb4cb11
ricardotmendes <ricardo.cau.mendes@gmail.com>
2020-11-03 15:34:05 -0300

    Criação Nav Bar e Footer iniciais

--- EtotFrontEnd/src/app/footer/footer.component.ts
+++ EtotFrontEnd/src/app/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import{faInstagram} from '@fortawesome/free-brands-svg-icons'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  faInstagram = faInstagram
 
   constructor() { }
 
